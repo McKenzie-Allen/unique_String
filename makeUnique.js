8733ded38698ba2121199bac2f68fb5025f078aa
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-01-24 00:00:15 -0700

    code will git rid of duplicates in a string

--- makeUnique.js
+++ makeUnique.js
@@ -1,5 +1,16 @@
 // Write your solution below:
 
-function uniqueString(str) {
+function makeUnique(str) {
+    let uniqueStr = ""
 
-};
+    for (let i = 0; i < str.length; i++) {
+        if (uniqueStr.includes(str[i])) {
+            continue
+        } else {
+            uniqueStr += str[i]
+        }
+    }
+    return uniqueStr
+}
+
+console.log(makeUnique("aassddffjj"))
